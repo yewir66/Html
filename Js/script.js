248f3fd5c50c36795ea095e72e4537e95f81eed0
Yewir66 <yewir123@gmail.com>
2025-08-14 19:46:31 -0400

    Add navigation buttons and video functionality with new assets

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,43 @@
-const button =document.getElementById('button')
+const button = document.getElementById('Next');
+const button2 = document.getElementById('button2');
+const indexButton = document.getElementById('indexButton');
+const tiktokButton = document.getElementById('tiktokButton');
 
-button.addEventListener("click",Changebackground)
+button2.style.display = "none";
+indexButton.style.display = "none"; 
+tiktokButton.style.display = "none";
 
-function Changebackground(){
-  const video = document.getElementById('bg-video')
-  const source = video.children[0]
-  console.log(source.src)
-  source.setAttribute('src', '/videos/Cat Video.mp4')  
-  video.load()
+button.addEventListener("click", Changebackground);
+button2.addEventListener("click", reversebackground);
+
+function Changebackground() {
+  const video = document.getElementById('bg-video');
+  const source = video.children[0];
+  console.log(source.src);
+  source.setAttribute('src', '/videos/scene2.mp4');  
+  video.load();
+
+  button.style.display = "none";
+  button2.style.display = "block";
+  indexButton.style.display = "block";
+  tiktokButton.style.display = "block";  
 }
+
+function reversebackground() {
+  const video = document.getElementById('bg-video');
+  const source = video.children[0];
+  console.log(source.src);
+  source.setAttribute('src', '/videos/inverse.mp4');
+  video.load();
+
+  button2.style.display = "none";
+  button.style.display = "block";
+  indexButton.style.display = "none";
+  tiktokButton.style.display = "none"; 
+}
+
+
+
+
+
+
